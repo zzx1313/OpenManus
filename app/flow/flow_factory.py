82e31403572b35a565ce2bcc64181e6183529117
liangxinbing <1580466765@qq.com>
2025-03-21 17:37:16 +0800

    update structure of flow

--- app/flow/flow_factory.py
+++ app/flow/flow_factory.py
@@ -1,8 +1,13 @@
+from enum import Enum
 from typing import Dict, List, Union
 
 from app.agent.base import BaseAgent
-from app.flow.base import BaseFlow, FlowType
+from app.flow.base import BaseFlow
 from app.flow.planning import PlanningFlow
+
+
+class FlowType(str, Enum):
+    PLANNING = "planning"
 
 
 class FlowFactory:
